301371858f6f642c6dc5aa9481c5cb02f0872290
iamyuu <akuyusuf27@gmail.com>
2022-03-26 11:25:04 +0700

    add an exmpale
    ref: https://github.com/jacob-ebey/remix-cloudflare-demo/blob/main/app/routes/geolocation.tsx

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,52 @@
+import type { LoaderFunction } from "remix";
+import { json, useLoaderData } from "remix";
+import countries from "../lib/countries.json";
+
+export let loader: LoaderFunction = ({ request }) => {
+	let cf = (request as any).cf as IncomingRequestCfProperties;
+
+	let country = countries.find(c => c.cca2 === cf.country);
+
+	let formattedLocation = "";
+	if (cf.city) formattedLocation += cf.city + ", ";
+	if (cf.region) formattedLocation += cf.region + ", ";
+	formattedLocation += cf.country;
+
+	return json({
+		formattedLocation,
+		country,
+	});
+};
+
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+	let { formattedLocation, country } = useLoaderData();
+
+	return (
+		<main>
+			<h1>Geolocation</h1>
+
+			<p>
+				Show localized content based on information avaliable in the <code>Request.cf</code> object.
+			</p>
+
+			<p>
+				Location: {formattedLocation} {country.flag}
+			</p>
+
+			<p>Currencies</p>
+			<ul>
+				{Object.entries(country.currencies).map(([abbr, currency]: any) => (
+					<li key={abbr}>
+						{abbr}: {currency.name} ({currency.symbol})
+					</li>
+				))}
+			</ul>
+			<p>Languages</p>
+			<ul>
+				{Object.values(country.languages).map((name: any) => (
+					<li key={name}>{name}</li>
+				))}
+			</ul>
+		</main>
+	);
 }
